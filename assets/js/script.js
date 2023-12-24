981b1b5e39d750a4ea312b6f488cf3ce8254616e
LokeshNivash <nivashnisa@gmail.com>
2023-12-24 23:01:35 +0530

    s

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,7 +121,6 @@ for (let i = 0; i < filterBtn.length; i++) {
 }
 
 
-
 // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
